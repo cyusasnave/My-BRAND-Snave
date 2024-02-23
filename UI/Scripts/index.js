41d0c2a78cb62744dc6ea1a923d2c42f501a4118
CYUSA Snave <cyusasnaveee@gmail.com>
2024-02-23 13:36:54 +0200

    adminPanel.html: modified rich text editor to tinymce

---
+++ UI/Scripts/index.js
@@ -0,0 +1,25 @@
+const sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+
+function closemenu() {
+  sidemenu.style.right = "-80%";
+}
+
+function openTab(evt, tabName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(tabName).style.display = "grid";
+  evt.currentTarget.classList.add("active");
+}
+document.querySelector(".tablinks").click();
+
